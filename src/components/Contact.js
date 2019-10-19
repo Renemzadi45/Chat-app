9a0975b8c17600e83f3ad4ac9fda8ee888e7fb4b
REné-Marcel Zadi <rene.mzadi45@gmail.com>
2019-10-19 02:51:07 +0200

    React 05 fini

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online
+    };
+  }
+  render() {
     return (
-        <div className="Contact">
-
-            <img className="avatar" src={props.avatar} />
-
-            <div className="right-element">
-                <div className='name'>{props.name}</div>
-
-                <div className="status">
-                    <div className="status-text">
-                        <div className={props.online ? 'status-online' : 'status-offline'}></div>
-                        {props.online ? "online" : "offline"}
-
-                    </div>
-                </div>
-            </div>
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} />
+        <div>
+          <p className="name">{this.props.name}</p>
+          <div className="status">
+            <p className="status-text">
+              <span
+                className={
+                  this.state.online ? "status-online" : "status-offline"
+                }
+                onClick={event => {
+                  const newVal = !this.state.online;
+                  this.setState({ online: newVal });
+                }}
+              />
+              {this.state.online ? "online" : "offline"}
+            </p>
+          </div>
         </div>
+      </div>
     );
+  }
 }
-
 
 export default Contact;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,49 +1,43 @@
 // src/components/QuoteList.js
 import React from "react";
-import Contact from './Contact';
+import Contact from "./Contact";
 
 // An array of objects
-const quotes = [
+const contacts = [
   {
     online: true,
     name: "Maddy Sision",
-    avatar:
-    "https://randomuser.me/api/portraits/women/94.jpg" 
-      
+    avatar: "https://randomuser.me/api/portraits/women/94.jpg"
   },
   {
     online: false,
     name: "Joyce Jonathan",
-    avatar:
-    "https://randomuser.me/api/portraits/women/33.jpg" 
-},
+    avatar: "https://randomuser.me/api/portraits/women/33.jpg"
+  },
   {
     online: true,
     name: "Al Spielberg",
-    avatar:
-    "https://randomuser.me/api/portraits/men/17.jpg"    
-},
+    avatar: "https://randomuser.me/api/portraits/men/17.jpg"
+  },
   {
     online: true,
     name: "Wade Smith",
-    avatar:
-    "https://randomuser.me/api/portraits/men/14.jpg"
-},
+    avatar: "https://randomuser.me/api/portraits/men/14.jpg"
+  },
 
-{
+  {
     online: false,
     name: "Ray Magini",
-    avatar:
-    "https://randomuser.me/api/portraits/men/48.jpg"
-}
+    avatar: "https://randomuser.me/api/portraits/men/48.jpg"
+  }
 ];
 
-const QuoteList = () => (
+const ContactList = () => (
   <div>
-    {quotes.map(item => (
-      <Contact name = {item.name} avatar = {item.avatar} online = {item.online}/>
+    {contacts.map(item => (
+      <Contact name={item.name} avatar={item.avatar} online={item.online} />
     ))}
   </div>
 );
 
-export default QuoteList;
+export default ContactList;
